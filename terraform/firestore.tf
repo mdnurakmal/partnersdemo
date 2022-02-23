74c48d672474e7771e53235dc8451e75006d35ee
Nur Akmal <2183322C@student.tp.edu.sg>
2022-02-23 12:08:34 +0800

    Update firestore.tf

--- terraform/firestore.tf
+++ terraform/firestore.tf
@@ -3,4 +3,5 @@ resource "google_firestore_document" "partnersdemo-db" {
   collection  = "demos"
   document_id = "demos-test"
   fields      = "{\"something\":{\"mapValue\":{\"fields\":{\"akey\":{\"stringValue\":\"avalue\"}}}}}"
+    depends_on = [google_project_service.firestore]
 }
