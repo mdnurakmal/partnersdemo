5374775ea5a6f2dad570ac0fb9d625a4eb226510
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2022-03-04 15:37:46 +0800

    signup page done

--- src/app/apiservice.service.ts
+++ src/app/apiservice.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class ApiserviceService {
 
-  constructor() { }
+  constructor(private _http:HttpClient) { }
+
+  //connect frontend to backend
+
+  apiUrl = 'https://mysql-cloud-run-7teekrs4qq-uc.a.run.app'
+
+
+  //register user
+  registerUser(data:any):Observable<any>
+  {
+    return this._http.post(`${this.apiUrl}/user/register`,data)
+  }
+
+
+
+
+
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiserviceService } from '../apiservice.service';
+
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +12,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupForm : FormGroup
+  loading = false
+  submitted = false
+
+  constructor
+    (
+    private formBuilder: FormBuilder, 
+    private service: ApiserviceService,
+    private router: Router
+    ) 
+    { }
+
+  dataexists:any;
 
   ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      first_name:['', Validators.required],
+      last_name:['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password:['',[Validators.required, Validators.minLength(6)]]
+    })
   }
+
+  get f() { return this.signupForm.controls}
+
+  signUp(): void{
+
+    this.submitted = true
+
+    if (this.signupForm.invalid){
+      return
+    }
+
+    this.loading = true
+
+    this.service.registerUser(this.signupForm.value)
+      .subscribe(res =>{
+        console.log(`This is what it emits ${res})`)
+        this.router.navigate(['/user/login'])
+      },(err: HttpErrorResponse) =>{
+        console.log(`This is the error message:${err.error}`)
+        this.dataexists = err.error.message
+      })
+
+  }
+
 
 }
